b80d4f038c10bb5df5fed1363537a93513d6ca45
ThirtySomething <developer@derpaul.net>
2019-08-13 20:56:42 +0200

    Added comments

--- SynopackageUninstall.sh
+++ SynopackageUninstall.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-# 1st disable service
+echo "------------------------------------------------------------"
 echo "`date +'%Y%m%d-%H:%M:%S'`: Check for SynoFinder"
+# 1st: Check if 'Universal Search' is installed
 if synoservice --list-config | grep -Fq 'pkgctl-SynoFinder'; then    
 	echo "`date +'%Y%m%d-%H:%M:%S'`: Disable SynoFinder"
+	# 2nd disable service
 	synoservice --disable pkgctl-SynoFinder
 	echo "`date +'%Y%m%d-%H:%M:%S'`: Uninstall SynoFinder"
-	# 2nd uninstall package
+	# 3rd uninstall package
 	synopkg uninstall SynoFinder
 fi
