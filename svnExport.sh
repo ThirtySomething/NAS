620183bb65f2439c3f4618bcf94cc2e0034f7648
ThirtySomething <developer@derpaul.net>
2021-10-30 10:45:31 +0200

    Updated svnExport.sh in keeping n files.

--- svnExport.sh
+++ svnExport.sh
@@ -10,7 +10,7 @@
 # location of all repositories
 VAR_PATH_SVN=/volume1/subversion/*
 # maximum days to keep a backup
-INT_AGE=4
+INT_AGE=5
 # file extension
 STR_EXT=gz
 # current date
@@ -23,8 +23,8 @@ DIR_EXPORT=`realpath ${DIR_EXPORT}`
 #| Check for valid SVN repository                                             |
 #+----------------------------------------------------------------------------+
 function is_svn_repository {
-	svnlook info $1 > /dev/null 2>&1
-	echo $?
+    svnlook info $1 > /dev/null 2>&1
+    echo $?
 }
 
 #+----------------------------------------------------------------------------+
@@ -39,20 +39,23 @@ function get_svn_destination_name {
 #+----------------------------------------------------------------------------+
 function drop_old_exports {
     PATTERN=`basename $1`
-	# The + on mtime points out INT_AGE days AND OLDER
-	echo "`date +'%Y%m%d-%H:%M:%S'`: Delete backups older than ${INT_AGE} days"
-	find ${DIR_EXPORT} -name \"${PATTERN}-*.${STR_EXT}\" -ctime +${INT_AGE}
-	find ${DIR_EXPORT} -name "${PATTERN}-*.${STR_EXT}" -ctime +${INT_AGE} -delete
-	echo ""
+    FTK=`expr 1 + ${INT_AGE}`
+    # The solution for deletion all files older than x days is described here
+    # https://stackoverflow.com/questions/25785/delete-all-but-the-most-recent-x-files-in-bash/25789
+    echo "`date +'%Y%m%d-%H:%M:%S'`: Delete backups older than ${INT_AGE} days"
+    echo "ls ${DIR_EXPORT}/${PATTERN}*.${STR_EXT} -tp | grep -v '/$' | tail -n +${FTK} | tr '\n' '\0' | xargs -0 rm --"
+    ls ${DIR_EXPORT}/${PATTERN}*.${STR_EXT} -tp | grep -v '/$' | tail -n +${FTK}
+    ls ${DIR_EXPORT}/${PATTERN}*.${STR_EXT} -tp | grep -v '/$' | tail -n +${FTK} | tr '\n' '\0' | xargs -0 rm --
+    echo ""
 }
 
 #+----------------------------------------------------------------------------+
 #| Export SVN repository                                                      |
 #+----------------------------------------------------------------------------+
 function export_svn_repository {
-	VAR_DEST_NAME=$(get_svn_destination_name $1)
-	echo "`date +'%Y%m%d-%H:%M:%S'`: Dumping repo [$1] to [$VAR_DEST_NAME]"
-	svnadmin dump $1 | gzip > $VAR_DEST_NAME
+    VAR_DEST_NAME=$(get_svn_destination_name $1)
+    echo "`date +'%Y%m%d-%H:%M:%S'`: Dumping repo [$1] to [$VAR_DEST_NAME]"
+    svnadmin dump $1 | gzip > $VAR_DEST_NAME
 }
 
 #+----------------------------------------------------------------------------+
@@ -61,13 +64,13 @@ function export_svn_repository {
 echo "***** Start export of SVN repositories for date [$STR_DATE]"
 for VAR_CURRENT_DIR in $VAR_PATH_SVN
 do
-	if [[ $(is_svn_repository $VAR_CURRENT_DIR) -eq 1 ]]
-	then
-		echo "`date +'%Y%m%d-%H:%M:%S'`: Skip non SVN repository [$VAR_CURRENT_DIR]."
-		echo ""
-	else
-		export_svn_repository $VAR_CURRENT_DIR
-		drop_old_exports $VAR_CURRENT_DIR
-	fi
+    if [[ $(is_svn_repository $VAR_CURRENT_DIR) -eq 1 ]]
+    then
+        echo "`date +'%Y%m%d-%H:%M:%S'`: Skip non SVN repository [$VAR_CURRENT_DIR]."
+        echo ""
+    else
+        export_svn_repository $VAR_CURRENT_DIR
+        drop_old_exports $VAR_CURRENT_DIR
+    fi
 done
 echo ""
