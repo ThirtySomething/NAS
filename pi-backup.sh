1b893ce941f963883f73123bdf1caf919f373a0a
ThirtySomething <developer@derpaul.net>
2022-01-30 14:32:04 +0100

    CHG: Perform backup on pi itself on mounted share

--- pi-backup.sh
+++ pi-backup.sh
@@ -15,11 +15,13 @@ REM_USR=pi
 REM_SYS=192.168.71.7
 # Maximum number backups to keep
 INT_KEEP=5
-# file extension
+# Name of backup
+NAME_BACKUP=pi-hole
+# File extension
 STR_EXT=gz
 # current date
 STR_DATE=$(date +%Y-%m-%d)
-# get current name of backup folder
+# Get current name of backup folder
 DIR_BACKUP=`dirname $0`
 DIR_BACKUP=`realpath ${DIR_BACKUP}`
 
@@ -34,14 +36,14 @@ function get_backup_name {
 #| Delete backups older than specified age                                      |
 #+------------------------------------------------------------------------------+
 function drop_old_backups {
-    PATTERN=`basename ${1}`
-    FTK=`expr 1 + ${INT_KEEP}`
+    PATTERN="${DIR_BACKUP}/${1}*.${STR_EXT}"
+    FTK=$(expr 1 + ${INT_KEEP})
     # The solution for keep the last x files is described here
-    # https://stackoverflow.com/questions/25785/delete-all-but-the-most-recent-x-files-in-bash/25789
+    # https://stackoverflow.com/questions/25785/delete-all-but-the-most-recent-x-files-in-bash
     echo "`date +'%Y%m%d-%H:%M:%S'`: Keep the last ${INT_KEEP} backups"
-    # echo "ls ${DIR_BACKUP}/${PATTERN}*.${STR_EXT} -tp | grep -v '/$' | tail -n +${FTK} | tr '\n' '\0' | xargs -0 rm --"
-    ls ${DIR_BACKUP}/${PATTERN}*.${STR_EXT} -tp | grep -v '/$' | tail -n +${FTK}
-    ls ${DIR_BACKUP}/${PATTERN}*.${STR_EXT} -tp | grep -v '/$' | tail -n +${FTK} | tr '\n' '\0' | xargs -0 rm --
+    # echo "ls ${PATTERN} -tp | grep -v '/$' | tail -n +${FTK} | xargs -I {} rm -- {}"
+    ls ${PATTERN} -tp | grep -v '/$' | tail -n +${FTK}
+    ls ${PATTERN} -tp | grep -v '/$' | tail -n +${FTK} | xargs -I {} rm -- {}
     echo ""
 }
 
@@ -51,7 +53,9 @@ function drop_old_backups {
 function backup_sd_card {
     VAR_DEST_NAME=$(get_backup_name ${1})
     echo "`date +'%Y%m%d-%H:%M:%S'`: Backup SD card [${1}] to [${VAR_DEST_NAME}]"
-    ssh ${REM_USR}@${REM_SYS} "sudo dd if=/dev/mmcblk0 bs=1M | gzip -" | dd of=${VAR_DEST_NAME}
+    # ssh ${REM_USR}@${REM_SYS} "sudo dd if=/dev/mmcblk0 bs=1M | gzip -" | dd of=${VAR_DEST_NAME}
+    dd if=/dev/mmcblk0 bs=32M | gzip - | dd of=${VAR_DEST_NAME}
+    echo ""
 }
 
 #+------------------------------------------------------------------------------+
@@ -59,6 +63,6 @@ function backup_sd_card {
 #+------------------------------------------------------------------------------+
 echo "--------------------------------------------------------------------------------"
 echo "***** Start backup of SD card at [${STR_DATE}]"
-backup_sd_card "pi-hole"
-drop_old_backups ${VAR_CURRENT_DIR}
+backup_sd_card "${NAME_BACKUP}"
+drop_old_backups "${NAME_BACKUP}"
 echo ""
