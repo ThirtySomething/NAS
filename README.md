8b11f0fdbc3510e269fa457a3915b96914bc5c1b
ThirtySomething <developer@derpaul.net>
2022-04-16 02:26:58 +0200

    UPD: Updated svnExport.sh script and README.md

--- README.md
+++ README.md
@@ -30,23 +30,36 @@ To adjust to your needs you have to change
 
 ## svnExport.sh
 
-On my NAS I've got a SVN server running. To backup my repositories without
-knowing the number or names of them, I'm using this script.
+On my DIY-NAS I've got a [SCM-Manager][SCM] running in a docker container. To
+backup the SVN repositories without knowing the number or names of them, I'm
+using this script.
 
 ### Setup
 
-Before you can run the script, you have to adjust some variables. They are not passed as arguments. At the head of the script you can see
+Before you can run the script, you have to adjust some variables. They are not
+passed as arguments. At the head of the script you can see
 
 ```bash
 # location of all repositories
-VAR_PATH_SVN=/volume1/subversion/*
+VAR_PATH_SVN="/srv/dev-disk-by-uuid-44c66e2f-7122-4c7c-9b2a-258139e35584/docker/volumes/scmmanager/_data/repositories/*"
+# suffix for SCM organized repositories
+SUFFIX_SVN=data
+# filename containing real repository name
+META_SVN=metadata.xml
 # maximum days to keep a backup
-INT_AGE=7
+INT_AGE=5
+# file extension
+STR_EXT=gz
+# current date
+STR_DATE=$(date +%Y-%m-%d)
+# get current name of backup folder
+DIR_EXPORT=$(dirname "${0}")
+DIR_EXPORT=$(realpath "${DIR_EXPORT}")
 ```
 
-You have to change `VAR_PATH_SVN` to the location of your SVN root path where
-your repositories resides. Changing `INT_AGE` is responsible for keeping n
-versions of your backup files.
+You have to change `VAR_PATH_SVN` to the location of your repositories of
+SCM-Manager. The variable `INT_AGE` is responsible for keeping n versions of
+your backup files. All other variables are for internal usage.
 
 ### Running
 
@@ -54,7 +67,7 @@ I'm running this script using the integrated task scheduler. Manually create a
 job, run it as the user `root` and enter something like this:
 
 ```bash
-bash /volume1/homes/<USER>/<PATH>/svnExport.sh >> /volume1/homes/<USER>/<PATH>/svnExport.log 2>&1
+/bin/bash /<PATH-TO-SCRIPT>/svnExport.sh >> /<PATH-TO-SCRIPT>/svnExport.log 2>&1 &
 ```
 
 The exported files will locate in the same directory as this script. You may
@@ -65,7 +78,10 @@ need to change this...
 The process is just simple but I'll explain it anyway:
 
 * Loop over all directories located in `VAR_PATH_SVN`
-* In case of a SVN repository, perform a SVN dump and gzip the dump
+* Loop over all subdirectories of the path
+* In case of a SVN repository
+  * Retrieve name of repository from `metadata.xml`
+  * Perform a SVN dump and gzip the dump
   * The determination of a SVN repository requires root privileges
 * In case a repository is dumped, cleanup the outdated backups
 
@@ -86,6 +102,7 @@ bash /volume1/homes/<USER>/SynopackageUninstall.sh >> /volume1/homes/<USER>/Syno
 ```
 
 [DS411slim]: https://www.synology.com/en-global/company/news/article/Synology_Unveils_DiskStation_DS411slim
+[SCM]: https://scm-manager.org/
 [backup]: https://pixelfriedhof.com/raspberry-pi-remote-backup-ueber-ssh-per-terminal-anfertigen/
 [pihole]: https://pi-hole.net/
 [remote]: http://www.linuxproblem.org/art_9.html
--- svnExport.sh
+++ svnExport.sh
@@ -1,77 +1,95 @@
 #!/bin/bash
-#+----------------------------------------------------------------------------+
-#| Script to export configured svn repositories                               |
-#+----------------------------------------------------------------------------+
+#+-----------------------------------------------------------------------------+
+#| Script to export configured svn repositories                                |
+#+-----------------------------------------------------------------------------+
 # Comment line out for debugging purposes
 # set -x
-#+----------------------------------------------------------------------------+
-#| Variable definitions                                                       |
-#+----------------------------------------------------------------------------+
+#+-----------------------------------------------------------------------------+
+#| Variable definitions                                                        |
+#+-----------------------------------------------------------------------------+
 # location of all repositories
-VAR_PATH_SVN=/volume1/subversion/*
-# maximum number of backups to keep
-INT_KEEP=5
+VAR_PATH_SVN="/srv/dev-disk-by-uuid-44c66e2f-7122-4c7c-9b2a-258139e35584/docker/volumes/scmmanager/_data/repositories/*"
+# suffix for SCM organized repositories
+SUFFIX_SVN=data
+# filename containing real repository name
+META_SVN=metadata.xml
+# maximum days to keep a backup
+INT_AGE=5
 # file extension
 STR_EXT=gz
 # current date
 STR_DATE=$(date +%Y-%m-%d)
 # get current name of backup folder
-DIR_EXPORT=`dirname $0`
-DIR_EXPORT=`realpath ${DIR_EXPORT}`
+DIR_EXPORT=$(dirname "${0}")
+DIR_EXPORT=$(realpath "${DIR_EXPORT}")
 
-#+----------------------------------------------------------------------------+
-#| Check for valid SVN repository                                             |
-#+----------------------------------------------------------------------------+
+#+-----------------------------------------------------------------------------+
+#| Check for valid SVN repository                                              |
+#+-----------------------------------------------------------------------------+
 function is_svn_repository {
-    svnlook info $1 > /dev/null 2>&1
+    svnlook info "${1}" >/dev/null 2>&1
     echo $?
 }
 
-#+----------------------------------------------------------------------------+
-#| Create name for SVN directory for export                                   |
-#+----------------------------------------------------------------------------+
-function get_svn_destination_name {
-   echo "$DIR_EXPORT/`basename $1`-$STR_DATE.$STR_EXT"
+#+-----------------------------------------------------------------------------+
+#| Get base name for SVN repository                                            |
+#+-----------------------------------------------------------------------------+
+function get_svn_base_name {
+    REPOBASE="$(dirname "${1}")/${META_SVN}"
+    REPONAME=$(xmlstarlet sel -T -t -m "/repositories/name" -v "/repositories/name" < "${REPOBASE}")
+    echo "${REPONAME}"
 }
 
-#+----------------------------------------------------------------------------+
-#| Delete backups older than specified age                                    |
-#+----------------------------------------------------------------------------+
+#+-----------------------------------------------------------------------------+
+#| Create name for SVN respoistory for export                                  |
+#+-----------------------------------------------------------------------------+
+function get_svn_destination_name {
+    REPOBASE=$(get_svn_base_name "${1}")
+    echo "${DIR_EXPORT}/${REPOBASE}-${STR_DATE}.${STR_EXT}"
+}
+
+#+-----------------------------------------------------------------------------+
+#| Delete backups older than specified age                                     |
+#+-----------------------------------------------------------------------------+
 function drop_old_exports {
-    PATTERN=`basename $1`
-    FTK=`expr 1 + ${INT_KEEP}`
-    # The solution for keep the last x files is described here
-    # https://stackoverflow.com/questions/25785/delete-all-but-the-most-recent-x-files-in-bash/25789
-    echo "`date +'%Y%m%d-%H:%M:%S'`: Keep the last ${INT_KEEP} backups"
-    # echo "ls ${DIR_EXPORT}/${PATTERN}*.${STR_EXT} -tp | grep -v '/$' | tail -n +${FTK} | tr '\n' '\0' | xargs -0 rm --"
-    ls ${DIR_EXPORT}/${PATTERN}*.${STR_EXT} -tp | grep -v '/$' | tail -n +${FTK}
-    ls ${DIR_EXPORT}/${PATTERN}*.${STR_EXT} -tp | grep -v '/$' | tail -n +${FTK} | tr '\n' '\0' | xargs -0 rm --
+    PATTERN=$(get_svn_base_name "${1}")
+    FTK=$((1 + "${INT_AGE}"))
+    echo "$(date +'%Y%m%d-%H:%M:%S'): Keep the last ${INT_AGE} backups"
+    COUNTER=0
+    for CURRENT_DUMP in $(find "${DIR_EXPORT}" -name "${PATTERN}-*.${STR_EXT}" | sort -nr); do
+        if [[ "${COUNTER}" -lt "${FTK}" ]]; then
+            echo "Keep dump [${CURRENT_DUMP}]"
+        else
+            echo "Delete dump [${CURRENT_DUMP}]"
+            rm "${CURRENT_DUMP}"
+        fi
+        COUNTER=$((COUNTER + 1))
+    done
     echo ""
 }
 
-#+----------------------------------------------------------------------------+
-#| Export SVN repository                                                      |
-#+----------------------------------------------------------------------------+
+#+-----------------------------------------------------------------------------+
+#| Export SVN repository                                                       |
+#+-----------------------------------------------------------------------------+
 function export_svn_repository {
-    VAR_DEST_NAME=$(get_svn_destination_name $1)
-    echo "`date +'%Y%m%d-%H:%M:%S'`: Dumping repo [$1] to [$VAR_DEST_NAME]"
-    svnadmin dump $1 | gzip > $VAR_DEST_NAME
+    VAR_DEST_NAME=$(get_svn_destination_name "${1}")
+    echo "$(date +'%Y%m%d-%H:%M:%S'): Dumping repo [${1}] to [${VAR_DEST_NAME}]"
+    svnadmin dump "${1}" | gzip > "${VAR_DEST_NAME}"
 }
 
-#+----------------------------------------------------------------------------+
-#| Loop over all repositories                                                 |
-#+----------------------------------------------------------------------------+
+#+-----------------------------------------------------------------------------+
+#| Loop over all repositories                                                  |
+#+-----------------------------------------------------------------------------+
 echo "--------------------------------------------------------------------------------"
-echo "***** Start export of SVN repositories for date [$STR_DATE]"
-for VAR_CURRENT_DIR in $VAR_PATH_SVN
-do
-    if [[ $(is_svn_repository $VAR_CURRENT_DIR) -eq 1 ]]
-    then
-        echo "`date +'%Y%m%d-%H:%M:%S'`: Skip non SVN repository [$VAR_CURRENT_DIR]."
+echo "***** Start export of SVN repositories for date [${STR_DATE}]"
+for VAR_CURRENT_DIR in ${VAR_PATH_SVN}; do
+    SVN_REPO="${VAR_CURRENT_DIR}/${SUFFIX_SVN}"
+    if [[ $(is_svn_repository "${SVN_REPO}") -eq 1 ]]; then
+        echo "$(date +'%Y%m%d-%H:%M:%S'): Skip non SVN repository [${SVN_REPO}]."
         echo ""
     else
-        export_svn_repository $VAR_CURRENT_DIR
-        drop_old_exports $VAR_CURRENT_DIR
+        export_svn_repository "${SVN_REPO}"
+        drop_old_exports "${SVN_REPO}"
     fi
 done
 echo ""
